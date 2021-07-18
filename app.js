d8e133ce6ad5733ff19413276a967ec7cd34df5f
marco <m.cruzado@rpalatam.com.pe>
2021-07-17 22:31:26 -0500

    fix .gitignore

--- app.js
+++ app.js
@@ -5,25 +5,23 @@ const multer = require('multer');
 const path = require('path');
 const app = express();
 
-
-
 //TODO: configuraciones
-app.set('port',process.env.PORT || 3000);
-app.set('views', path.join(__dirname,'/views'));
-app.set('view engine', 'ejs' );
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
 
 
 //TODO:middleawares
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
 //TODO: rutas
-app.use('/api',require('./routes/index.routes'));
+app.use('/api', require('./routes/index.routes'));
 
 //TODO: archivos estaticos
-app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 //TODO:exprtando el modulo app
 module.exports = app;
