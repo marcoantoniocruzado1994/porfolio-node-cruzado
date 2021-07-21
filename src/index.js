bcf767c2a762f2db83ee24e4e6012577c4bbc5d0
marco <m.cruzado@rpalatam.com.pe>
2021-07-21 09:45:29 -0500

    moved files into src

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ require('./db/database');
 
 
 
-//TODO: inicializacion del servidor
+//TODO: initialization del servidor
 app.listen(app.get('port'),()=>{
     console.log("Server on port" ,app.get('port') );
 })
