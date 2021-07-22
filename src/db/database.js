ffa9ea9b7bacacbc1e760a1ca062d08496e582de
marco <m.cruzado@rpalatam.com.pe>
2021-07-21 22:36:28 -0500

    fuite

--- src/db/database.js
+++ src/db/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.URL_DATABASE,{
+mongoose.connect('mongodb+srv://marco:marco@cluster0.rqfqx.mongodb.net/portafolio-demo?retryWrites=true&w=majority',{
 useNewUrlParser:true,
 useUnifiedTopology:true,
 useFindAndModify:false
